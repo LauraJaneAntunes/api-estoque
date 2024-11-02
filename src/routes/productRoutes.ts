86f0af5d3ba7714529c658fe4be0ebb4c640292a
Laura <laura.antunes@gmail.com>
2024-11-02 08:06:49 -0300

    add rota de delete todos os produtos

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -3,10 +3,11 @@ import { validationResult } from 'express-validator';
 import Product from '../models/productModel';
 import Stock from '../models/stockModel';
 import productValidation from '../middleware/productValidation';
+import authenticate from '../middleware/authenticate';
 
 const router = express.Router();
 
-router.post('/', productValidation, async (req:any, res:any) => {
+router.post('/', authenticate, productValidation, async (req:any, res:any) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
@@ -24,7 +25,7 @@ router.post('/', productValidation, async (req:any, res:any) => {
   }
 });
 
-router.get('/', async (req:any, res:any) => {
+router.get('/', authenticate, async (req:any, res:any) => {
   try {
     const products = await Product.findAll();
     return res.json(products);
@@ -33,7 +34,7 @@ router.get('/', async (req:any, res:any) => {
   }
 });
 
-router.get('/:id', async (req:any, res:any) => {
+router.get('/:id', authenticate, async (req:any, res:any) => {
   const { id } = req.params;
   try {
     const product = await Product.findByPk(id);
@@ -46,7 +47,7 @@ router.get('/:id', async (req:any, res:any) => {
   }
 });
 
-router.put('/:id', productValidation, async (req:any, res:any) => {
+router.put('/:id', authenticate, productValidation, async (req:any, res:any) => {
   const { id } = req.params;
   const { name, description, price } = req.body;
 
@@ -63,7 +64,7 @@ router.put('/:id', productValidation, async (req:any, res:any) => {
   }
 });
 
-router.delete('/:id', async (req:any, res:any) => {
+router.delete('/:id', authenticate, async (req:any, res:any) => {
   const { id } = req.params;
   try {
     // Exclui os registros vinculados na tabela `stocks`
@@ -73,7 +74,30 @@ router.delete('/:id', async (req:any, res:any) => {
     return res.status(204).send();
   } catch (error) {
     console.error('Error deleting product:', error);
-    return res.status(500).json({ error: 'Erro ao deletar produto' });
+    return res.status(500).json({ error: 'Erro ao deletar o produto' });
+  }
+});
+
+router.delete('/', authenticate, async (req: any, res: any) => {
+  const { ids } = req.body;
+  if (!ids || !Array.isArray(ids)) {
+    return res.status(400).send('IDs são necessários');
+  }
+
+  try {
+    await Stock.destroy({ 
+      where: { 
+        productId: ids 
+      } 
+    });
+    await Product.destroy({
+      where: {
+        id: ids
+      }
+    });
+    res.send('Produtos deletados com sucesso.');
+  } catch (error) {
+    res.status(500).send('Erro ao deletar esses produtos.');
   }
 });
 
